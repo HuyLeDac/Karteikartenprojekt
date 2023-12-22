1417528841239e8ac504101661c11907ca06f1e3
HuyLeDac <huyledac04@gmail.com>
2023-12-22 23:37:25 +0100

    Anmeldedatensystem ergänzt

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import json
+import os
 from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__, template_folder='templates')
+
+# Laden der Dummy-Anmeldedaten aus der JSON-Datei
+json_file_path = os.path.join(os.path.dirname(__file__), "anmeldedaten_dummy.json")
+# Laden der Dummy-Anmeldedaten aus der JSON-Datei
+with open(json_file_path) as json_file:
+    anmeldedaten = json.load(json_file)
+
 
 @app.route('/')
 def welcome_page():
@@ -22,6 +31,25 @@ def register_page():
         return jsonify({'redirect': '/register'})
     else:
         return render_template('register.html')
+
+@app.route('/check_anmeldung', methods=['POST'])
+def check_anmeldung():
+    benutzername = request.form.get('benutzername')
+    passwort = request.form.get('passwort')
+
+    # Überprüfe die Anmeldedaten
+    if benutzername in anmeldedaten and anmeldedaten[benutzername]['passwort'] == passwort:
+        return jsonify({'success': True, 'message': 'Anmeldung erfolgreich!', 'redirect': '/dashboard'})
+    else:
+        return jsonify({'success': False, 'message': 'Ungültige Anmeldedaten.'})
+
+
+#TODO: Route für Resgistrierung
+
+@app.route('/dashboard')
+def dashboard_page():
+    return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/scripts/welcome.js
+++ static/scripts/welcome.js
@@ -15,3 +15,25 @@ function goToRegister() {
         })
         .catch(error => console.error('Error:', error));
 }
+
+function submitAnmeldung() {
+    var benutzername = document.getElementById('benutzername').value;
+    var passwort = document.getElementById('passwort').value;
+
+    $.ajax({
+        type: 'POST',
+        url: '/check_anmeldung',
+        data: { 'benutzername': benutzername, 'passwort': passwort },
+        success: function(response) {
+            if (response.success) {
+                alert(response.message);
+                window.location.href = response.redirect;
+            } else {
+                alert(response.message);
+            }
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
